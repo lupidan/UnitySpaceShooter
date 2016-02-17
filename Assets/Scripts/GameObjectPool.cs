278c5f9caa6165b79e1ad9f3de0aa76ed49db1ed
Daniel Lupiañez Casares <lupidan@gmail.com>
2016-02-18 00:17:04 +0100

    Added first implementation of Game Object Pool

---
+++ Assets/Scripts/GameObjectPool.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public interface IPooledObject
+{
+    void OnSpawn();
+    void OnDespawn();
+}
+
+public class ObjectPool {
+
+    private Queue<GameObject> pooledObjects;
+
+    public GameObject pooledPrefab { get; private set; }
+    
+    public ObjectPool(GameObject gameObject)
+    {
+        pooledObjects = new Queue<GameObject>();
+        pooledPrefab = gameObject;
+    }
+
+    public GameObject SpawnObject()
+    {
+        GameObject gameObject = null;
+        if (pooledObjects.Count > 0)
+        {
+            gameObject = pooledObjects.Dequeue();
+        }
+        else
+        {
+            gameObject = GameObject.Instantiate(pooledPrefab);
+        }
+        gameObject.SetActive(true);
+
+        IPooledObject[] pooledObjectComponents = gameObject.GetComponents(typeof(IPooledObject)) as IPooledObject[];
+        foreach (IPooledObject pooledObjectComponent in pooledObjectComponents)
+        {
+            pooledObjectComponent.OnSpawn();
+        }
+
+        return gameObject;
+    }
+
+    public bool RecycleObject(GameObject gameObject)
+    {
+        pooledObjects.Enqueue(gameObject);
+        gameObject.SetActive(false);
+
+        IPooledObject[] pooledObjectComponents = gameObject.GetComponents(typeof(IPooledObject)) as IPooledObject[];
+        foreach (IPooledObject pooledObjectComponent in pooledObjectComponents)
+        {
+            pooledObjectComponent.OnSpawn();
+        }
+
+        return true;
+    }
+}
