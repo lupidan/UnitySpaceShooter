792cec5773e4c3ae2e2255152083b56f81a1e2ab
Daniel Lupiañez Casares <lupidan@gmail.com>
2016-02-18 20:52:43 +0100

    Adding ship, starting some behaviour. Adding some UI elements

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 inputVector = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        inputVector *= 5.0f;
+        transform.position += inputVector * Time.deltaTime;
+	}
+}
