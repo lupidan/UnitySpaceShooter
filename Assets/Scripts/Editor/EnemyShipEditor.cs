b4fe186dbb9e4a80453c9f36ed66f3690258b525
Daniel Lupiañez Casares <lupidan@gmail.com>
2016-02-25 22:35:03 +0100

    feat/EditorScriptsForBulletAndShips: Moved functionality.

---
+++ Assets/Scripts/Editor/EnemyShipEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(EnemyShip))]
+public class EnemyShipEditor : Editor {
+
+    [DrawGizmo(GizmoType.Selected)]
+	static void DrawInterestingAreas(EnemyShip enemyShip, GizmoType gizmoType)
+    {
+        Gizmos.color = Color.red;
+        enemyShip.activeArea.DrawGizmo();
+        Gizmos.color = Color.magenta;
+        enemyShip.shootingArea.DrawGizmo();
+    }
+
+}
---
+++ Assets/Scripts/Editor/PlayerShipEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PlayerShip))]
+public class PlayerShipEditor : Editor {
+
+    [DrawGizmo(GizmoType.Selected)]
+	static void DrawInterestingAreas(PlayerShip playerShip, GizmoType gizmoType)
+    {
+        Gizmos.color = Color.red;
+        playerShip.gameArea.DrawGizmo();
+    }
+
+}
